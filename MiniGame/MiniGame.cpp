06b48f658c3e1643617cb4d8ca5a3c15d42b0dcf
Kilan <kilan22042009@gmail.com>
2026-07-15 23:26:24 +0300

    Added different enemies with different stats and made them kinda balanced for different gamemodes

--- MiniGame/MiniGame.cpp
+++ MiniGame/MiniGame.cpp
@@ -12,13 +12,23 @@ using std::cout, std::cin, std::string, std::endl;
 void playerCreation(Player& myPlayer);
 int main()
 {
-    
     srand(time(0));
 
-
     Player myPlayer = Player(100, 20, 50, 25, 2);
+    Enemy* pCurrentEnemy;
+    Enemy rat = Enemy("Rat", 25, 5, 10, 10, 0, 5, 10);
     Enemy spider = Enemy("Spider", 50, 15, 25, 50, 0, 10, 20);
+    Enemy rogue = Enemy("Rogue", 75, 25, 35, 15, 0, 10, 20);
+    Enemy skeleton = Enemy("Skeleton", 75, 20, 30, 60, 0, 15, 30);
     Enemy zombie = Enemy("Zombie", 100, 20, 30, 15, 1, 15, 30);
+    Enemy goblin = Enemy("Goblin", 75, 30, 40, 10, 0, 25, 30);
+    Enemy barbarian = Enemy("Barbarian", 100, 20, 40, 5, 1, 30, 50);
+    Enemy bear = Enemy("Bear", 75, 40, 70, 25, 2, 55, 75);
+    Enemy juggernaut = Enemy("Juggernaut", 250, 15, 20, 10, 3, 50, 100);
+    Enemy knight = Enemy("Knight", 150, 40, 65, 25, 2, 75, 100);
+    Enemy dragon = Enemy("Dragon", 300, 30, 60, 35, 1, 100, 120);
+    Enemy demon = Enemy("Demon", 400, 60, 100, 40, 1, 120, 200);
+
     
     int experienceAddTest = 10;
     while (true) {
